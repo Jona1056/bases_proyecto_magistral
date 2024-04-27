7b34a0237eaf3d45d18969a2e2206c8166b947ce
DayanaReyes <3533294870101@ingenieria.usac.edu.gt>
2024-04-27 00:15:36 -0600

    consultas 2, 5 y 6

--- Consultas.sql
+++ Consultas.sql
@@ -1,5 +1,16 @@
 -- consulta 1
-
+-- consulta 2
+SELECT 
+    p.nombre AS nombre_pais,
+    d.nombre AS nombre_departamento,
+    SUM(CASE WHEN c.sexo_id_genero = 1 THEN 1 ELSE 0 END) AS total_votos_mujeres,
+    (SUM(CASE WHEN c.sexo_id_genero = 1 THEN 1 ELSE 0 END) / SUM(CASE WHEN c.sexo_id_genero IN (1, 2) THEN 1 ELSE 0 END)) * 100 AS porcentaje_votos_mujeres
+FROM resultados r
+INNER JOIN zona z ON r.zona_id_zona = z.id_zona
+INNER JOIN departamento d ON z.departamento_id_departamento = d.id_departamento
+INNER JOIN pais p ON z.pais_id_pais = p.id_pais
+INNER JOIN caracteristicas c ON r.caracteristicas_id_caracteristicas = c.id_caracteristicas
+GROUP BY p.nombre, d.nombre;
 -- consulta 3
 SELECT 
     p.nombre AS 'Nombre del País',
@@ -24,6 +35,40 @@ INNER JOIN (
 ) AS max_resultado ON r.zona_id_zona = max_resultado.zona_id_zona AND r.id_resultado = max_resultado.max_id_resultado
 GROUP BY 
     p.nombre, partido.nombre;
+
+-- consulta 5
+SELECT 
+    d.nombre AS nombre_departamento,
+    ((COUNT(CASE WHEN c.sexo_id_genero = 1 AND c.universitarios = 1 THEN r.id_resultado END) / COUNT(CASE WHEN c.sexo_id_genero = 1 THEN r.id_resultado END)) * 100) AS porcentaje_mujeres_universitarias,
+    ((COUNT(CASE WHEN c.sexo_id_genero = 2 AND c.universitarios = 1 THEN r.id_resultado END) / COUNT(CASE WHEN c.sexo_id_genero = 2 THEN r.id_resultado END)) * 100) AS porcentaje_hombres_universitarios
+FROM resultados r
+INNER JOIN zona z ON r.zona_id_zona = z.id_zona
+INNER JOIN departamento d ON z.departamento_id_departamento = d.id_departamento
+INNER JOIN caracteristicas c ON r.caracteristicas_id_caracteristicas = c.id_caracteristicas
+GROUP BY d.nombre
+HAVING porcentaje_mujeres_universitarias > porcentaje_hombres_universitarios;
+
+
+-- consulta 6
+SELECT 
+    p.nombre AS nombre_pais,
+    r.nombre AS nombre_region,
+    AVG(total_votos_por_departamento) AS promedio_votos_por_departamento
+FROM (
+    SELECT 
+        z.pais_id_pais,
+        z.region_id_region,
+        d.id_departamento,
+        COUNT(r.id_resultado) AS total_votos_por_departamento
+    FROM zona z
+    INNER JOIN departamento d ON z.departamento_id_departamento = d.id_departamento
+    LEFT JOIN resultados r ON z.id_zona = r.zona_id_zona
+    GROUP BY z.pais_id_pais, z.region_id_region, d.id_departamento
+) AS votos_por_departamento
+INNER JOIN pais p ON votos_por_departamento.pais_id_pais = p.id_pais
+INNER JOIN region r ON votos_por_departamento.region_id_region = r.id_region
+GROUP BY p.nombre, r.nombre;
+
 
 -- consulta 7
 SELECT
